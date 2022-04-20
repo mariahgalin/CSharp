64f63aa21fe5d09366a18e00b77601fe31062f35
Maria Montoya <mariag.gmontoya@gmail.com>
2022-04-20 17:53:54 -0500

    2nd section T6 and T7

---
+++ IfSentence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IfSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool areYouHungry = true;
+            bool youHaveMoney = true;
+
+            if (areYouHungry && youHaveMoney && IsOpenRestaurant("Carls Jr", 12))
+            {
+                Console.WriteLine("Eat!");
+            }
+            else
+            {
+                Console.WriteLine("Dont eat :( ");
+            }
+
+            Console.ReadKey();
+           
+        }
+        static bool IsOpenRestaurant(string name, int hour = 0)
+        {
+            if (name == "Carls Jr" && hour > 8 && hour <23)
+            {
+                return true;
+            }
+            else if (name == "Restaurant 24 hrs")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Switch sentence/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Switch_sentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int op = 5;
+
+            switch (op)
+            {
+                case 1:
+                    Console.WriteLine("You have been selected case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("You have been selected case 2");
+                    break;
+                case 3:
+                case 4:
+                    Console.WriteLine("You have been selected cases 3 & 4");
+                    break;
+                //case < 0:
+                //    Console.WriteLine("Out of range");
+                //    break;
+                //case > 4 and 10;
+                //    Console.WriteLine("You have been selected between 4 and 10");
+                //    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+                    
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
