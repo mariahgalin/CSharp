6f77f50661ab339ef3f79c853b5ebac47b79ece5
Maria Montoya <mariag.gmontoya@gmail.com>
2022-04-21 17:53:38 -0500

    2nd Section T8 and T9

---
+++ Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friend = new string[5];
+
+            friend[0] = "Marie";
+            Console.WriteLine(friend[0]);
+
+            string[] michis = new string[5]
+            {
+                "Bernardino",
+                "Gaston",
+                "Dante",
+                "Tommas",
+                null
+            };
+            
+
+            michis[4] = "Romeo";
+
+            Console.WriteLine(michis[4]);
+
+
+            int index = 0;
+
+            while (index < michis.Length) 
+            {
+                Console.WriteLine(michis[index]);
+                index++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WhileDoWhileSentence/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WhileDoWhileSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 10)
+            {
+                Console.WriteLine("Iteration of X " + x);
+                // x = x + 1;
+                x++;
+            }
+
+
+            int y = 0;
+            while (y < 50)
+            {
+                if (y > 30)
+                    break;
+
+                Console.WriteLine("Iteration of Y " + y);
+                y++;
+            }
+
+            bool run = false;
+            do
+            {
+                Show();
+
+            } while (run);
+
+            Console.ReadKey();
+        }
+
+        static void Show()
+        {
+            Console.WriteLine("Just for once");
+        }
+    }
+}
