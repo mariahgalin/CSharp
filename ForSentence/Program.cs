ea4078f83ee1707ca53e3869b60e32176f1e41dc
Maria Montoya <mariag.gmontoya@gmail.com>
2022-05-02 17:36:36 -0500

    2nd Section T10

---
+++ ForSentence/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ForSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] michis = new string[5]
+          {
+                "Johnny",
+                "Bernardino",
+                "Gaston",
+                "Dante",
+                "Romeo"
+          };
+
+            bool run = true;
+
+            for (int i = 0; i < michis.Length && run; i++)
+            {
+                Console.WriteLine(michis[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
