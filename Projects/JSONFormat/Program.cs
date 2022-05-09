9f475f38f743bc67be57eec6bc4578e962f43069
Maria Montoya <mariag.gmontoya@gmail.com>
2022-05-09 15:31:55 -0500

    3rd Section T19

---
+++ Projects/JSONFormat/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace JSONFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wine myWine = new Wine()
+            {
+                Name = "Conserva Real",
+                Brand = "Torres"
+            };
+
+            string json = "{\"Name\": \"Conserva Real\", \"Brand\": \"Torres\"}";
+            
+
+            Wine[] Wines = new Wine[]
+            {
+                new Wine()
+                {
+                    Name = "Conserva Real",
+                    Brand = "Torres"
+                },
+                new Wine()
+                {
+                    Name = "Claro de Luna",
+                    Brand = "Cabernete"
+                }
+            };
+
+            string json2 = "[" +
+                "{\"Name\": \"Conserva Real\", \"Brand\": \"Torres\"}" +
+                "{\"Name\": \"Claro de Luna\", \"Brand\": \"Cabernete\"}" +
+                "]";
+            
+        }
+    }
+
+    public class Wine
+    {
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
