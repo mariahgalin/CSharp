a0f24ae507d808fb7d40713e9405ff92c342963f
María Montoya <mariag.gmontoya@gmail.com>
2022-05-12 17:46:41 -0500

    4th section T21 and T22

---
+++ Projects/ForEachSentence/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ForEachSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para agregar namespace necesarios
+            //Control + .
+
+            var numbers = new List<int>()
+            {
+                6,3,886,432,0
+            };
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //List<People> students = new List<People>()
+            var students = new List<People>()
+            {
+                new People() {Name = "Maria", Country="Mexico"},
+                new People() {Name = "Amanda", Country="Brazil"}
+            };
+
+            Show(students);
+
+            Console.ReadKey();
+
+        }
+
+        static void Show(List<People> students)
+        {
+            Console.WriteLine("--- People ---");
+
+            foreach(var s in students)
+            {
+                Console.WriteLine($"Nombre: {s.Name}, País: {s.Country}");
+            }
+        }
+    }
+
+    class People
+    {
+        public string Name { get; set; }
+
+        public string Country { get; set; }
+    }
+}
---
+++ Projects/Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(4);
+            numbers.Add(5);
+
+            Console.WriteLine(numbers.Count);
+
+            List<int> numbers2 = new List<int>()
+            {
+                14,8,9,2
+            };
+            Console.WriteLine(numbers2.Count);
+
+            numbers2.Clear();
+            Console.WriteLine(numbers2.Count);
+
+            List<string> countries = new List<string>()
+            {
+                "Mexico", "Colombia", "Peru"
+            };
+            Console.WriteLine(countries.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
