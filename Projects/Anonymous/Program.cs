91c5add1cb93f651b36fd493fbc2d6f1f4a93089
María Montoya <mariag.gmontoya@gmail.com>
2022-05-14 22:23:52 -0500

    5th Section T24

---
+++ Projects/Anonymous/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Anonymous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maria = new
+            {
+                Name = "Maria",
+                Country = "Aguascalientes"
+            };
+
+            Console.WriteLine($"{maria.Name} {maria.Country}");
+
+            var beers = new[] 
+            {
+                new { Name = "Red", Brand = "Delirium" }, 
+                new { Name = "London Porter", Brand = "Fullers"}
+            };
+
+            foreach (var b in beers)
+            {
+                Console.WriteLine($"Beer {b.Name} Brand {b.Brand}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
