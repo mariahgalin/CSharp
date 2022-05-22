5170dd99b8c001e909d31e36231864e7db6c7aba
María Montoya <mariag.gmontoya@gmail.com>
2022-05-21 20:57:21 -0500

    6th Section T26 and T27

---
+++ Projects/Exceptions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Using complete path, it is important using @ before the path
+                string content = File.ReadAllText("test.txt");
+                Console.WriteLine(content);
+
+                string content2 = File.ReadAllText("test2.txt");
+                Console.WriteLine(content);
+
+                throw new Exception("Something weird happend");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("The file does not exist");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("This is executing anyway");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projects/PersonalizedExceptions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PersonalizedExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Wine wine = new Wine()
+                {
+                    Name = "Cabernet Sauvignon",
+                   // Brand = "Gato Negro"
+                };
+
+                Console.WriteLine(wine);
+            }
+            catch (InvalidWineException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class InvalidWineException : Exception
+    {
+        public InvalidWineException(): base("The wine does not have name or brand, so it is invalid.")
+        {
+
+        }
+    }
+    public class Wine
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+
+        public override string ToString()
+        {
+            if (Name == null || Brand == null)
+                throw new InvalidWineException();
+
+            //return base.ToString();
+            return $"Wine: {Name}, Brand: {Brand}";
+        }
+    }
+}
