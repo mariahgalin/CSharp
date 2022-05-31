9a63f18eea274e552764b67ecd78f4aafa0b9b19
María Montoya <mariag.gmontoya@gmail.com>
2022-05-31 17:56:05 -0500

    7th Section T28 and T29

---
+++ Projects/LINQ/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Wine> wine = new List<Wine>()
+            {
+                new Wine()
+                {
+                    Name="Viñedo Chadwick", Country="Chile"
+                },
+                new Wine()
+                {
+                    Name="Finca Las Moras", Country= "Argentina"
+                },
+                 new Wine()
+                {
+                    Name="Quinta Monasterio", Country="México"
+                }
+            };
+
+            foreach (var w in wine)
+                Console.WriteLine(w);
+
+            Console.WriteLine("------------------------------------------");
+
+            //select
+            var wineName = from w in wine
+                           select new
+                           {
+                               Name = w.Name,
+                               Letters = w.Name.Length,
+                               Fixed = 1
+                           };
+            foreach (var wn in wineName)
+                Console.WriteLine($"{wn.Name} {wn.Letters} {wn.Fixed}");
+
+            Console.WriteLine("------------------------------------------");
+
+            var getWineName = from w in wineName
+                           select new
+                           {
+                               Name = w.Name
+                           };
+
+            foreach (var gwn in getWineName)
+                Console.WriteLine($"{gwn.Name}");
+
+            Console.WriteLine("------------------------------------------");
+
+            var wineMéxico = from w in wine
+                              where w.Country == "México"
+                              || w.Country == "Argentina"
+                             select w;
+            
+            foreach(var w in wineMéxico)
+                Console.WriteLine(w);
+
+            Console.WriteLine("------------------------------------------");
+
+            var orderedWine = from w in wine
+                              orderby w.Country descending
+                              select w;
+
+            foreach (var ow in orderedWine)
+            Console.WriteLine(ow);
+
+     Console.WriteLine("------------------------------------------");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Wine
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Country: {Country}";
+        }
+    }
+}
---
+++ Projects/LINQ_Join/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wine = new List<Wine>()
+            {
+                new Wine()
+                {
+                    Name="Cirsion", Country="España"
+                },
+                new Wine()
+                {
+                    Name="Quinta Monasterio", Country="México"
+                },
+                new Wine()
+                {
+                    Name="Mayra", Country="Turquía"
+                }
+            };
+
+            var countries = new List<Country>()
+            {
+                new Country()
+                {
+                    Name="Mexico", Continent="America"
+                },
+                new Country()
+                {
+                    Name="España", Continent="Europa"
+                },
+                new Country()
+                {
+                    Name="Turquia", Continent="Asia"
+                }
+            };
+
+            var wineWithContinent = from w in wine
+                                    join country in countries
+                                    on w.Country equals country.Name
+                                    select new
+                                    {
+                                        Name = w.Name,
+                                        Country = w.Country,
+                                        Continent = country.Continent
+                                    };
+
+            foreach(var w in wineWithContinent)
+            {
+                Console.WriteLine($"{w.Name} {w.Country} {w.Continent}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Wine
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+
+    class Country
+    {
+        public string Name { get; set; }
+        public string Continent { get; set; }
+    }
+}
