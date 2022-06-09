7bb2608cc39e4ada05e17fe61122d61567215873
María Montoya <mariag.gmontoya@gmail.com>
2022-06-09 17:45:59 -0500

    8th Section T32

---
+++ Projects/Database/Wine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public class Wine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int BrandId { get; set; }
+
+        public Wine(int id, string name, int brandId)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.BrandId = brandId;
+        }
+    }
+}
---
+++ Projects/Database/WineDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Database
+{
+    public class WineDB : DB
+    {
+        public WineDB(string server, string dbName, string user, string password) : base (server, dbName, user, password)
+        {
+            
+        }
+
+        public List<Wine> GetAll()
+        {
+            Connect();
+
+            List<Wine> wine = new List<Wine>();
+            string query = "SELECT ID, Name, BrandID FROM Wine";
+            SqlCommand command = new SqlCommand(query, _connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                int id = reader.GetInt32(0);
+                string name = reader.GetString(1);
+                int brandId = reader.GetInt32(2);
+                wine.Add(new Wine(id, name, brandId));
+            }
+
+            Close();
+
+            return wine;
+        }
+    }
+}
