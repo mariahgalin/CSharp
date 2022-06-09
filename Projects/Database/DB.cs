8965d799e001a6c04e003c2a6bae3bb75cf31760
María Montoya <mariag.gmontoya@gmail.com>
2022-06-09 15:32:04 -0500

    8th Section T31

---
+++ Projects/Database/DB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Database
+{
+    public class DB
+    {
+        private string _connectionString;
+        protected SqlConnection _connection; 
+
+        public DB(string server, string dbName, string user, string password)
+        {
+            _connectionString = $"Data Source={server}; Initial Catalog={dbName};" +
+                $"User={user}; Password={password}; Trusted_Connection = True";
+        }
+
+        public void Connect()
+        {
+            _connection = new SqlConnection(_connectionString);
+            _connection.Open();
+        }
+
+        public void Close()
+        {
+            if(_connection != null && _connection.State == System.Data.ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+            
+        }
+    }
+}
---
+++ Projects/Database/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DB database = new DB(@"DESKTOP-QOKB6J4", "CSharpDB", "sa", "123456");
+
+                database.Connect();
+                Console.WriteLine("Successfully connected");
+
+                database.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine("Cannot connect"+ "\n\n" + e.Message);
+            }
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}
